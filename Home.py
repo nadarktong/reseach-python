7027af5ed2293ec275d37811398f31657d37e629
nadarktong <neangez@gmail.com>
2025-08-24 15:46:48 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.page_link("pages/KnnwithHeart.py", label="การวิเคราะห์การโจมตี Fishing ด้วยเทคนิค KNN", icon="1️⃣")
+st.page_link("pages/KnnwithHeart.py", label="การวิเคราะห์การโจมตี Fishing ด้วยเทคนิค KNNxxx", icon="1️⃣")
 st.page_link("pages/NaiveBaye.py", label="การวิเคราะห์การโจมตี Fishing ด้วยเทคนิค Naive Baye", icon="2️⃣", disabled=False)
 st.page_link("pages/DTree.py", label="การวิเคราะห์การโจมตีแบบ Fishing ด้วยเทคนิค Decision Tree", icon="1️⃣")
